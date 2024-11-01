36390228a1d69d13800046496f9037aace252bc3
Jawad Hossain Mahi <jawadhossainmahi@gmail.com>
2024-11-02 05:20:43 +0600

    image added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TextcomponentComponent,ImagecomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Jawad Hossain Mahi this is your first project';
 }
